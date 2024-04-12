500dbee1f1b16d4ee5313a8c1f001a00c6222ff4
Collibra <collibra@kamil-mbp.home>
2024-04-12 12:03:46 +0200

    Pierwszy commit

---
+++ src/test/java/tests/BasePage.java
@@ -0,0 +1,25 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    protected static final String BASE_URL = "http://automationpractice.pl/index.php";
+
+    protected WebDriver driver;
+
+    @BeforeAll
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
